e58cf883ecc22e754a1334dca4cffd54e1789c92
sailzhou <jiakang12@163.com>
2016-06-28 10:48:49 +0800

    Update qq.php

--- qq.php
+++ qq.php
@@ -1,6 +1,6 @@
 /*
 * qq登录授权过程中用到的回调地址，此文将回调地址单独作为一个文件存在
-* 
+* 回调地址用于获取Authorization Code，这里作为一个文件中转，方便后续的信息获取
 */
 
 header('location:'.create_url('mobile/login',array('api'=>'qq','code'=>$_GET['code'])));
